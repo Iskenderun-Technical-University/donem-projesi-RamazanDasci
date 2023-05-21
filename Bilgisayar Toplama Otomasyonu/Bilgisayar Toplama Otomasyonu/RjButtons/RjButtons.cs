7732eb1fb154fe54c2e5b0823580886d6843dbe5
Ramazan Daşcı <teknoyun655@gmail.com>
2023-05-21 18:43:33 +0300

    rjButtons eklendi

---
+++ Bilgisayar Toplama Otomasyonu/Bilgisayar Toplama Otomasyonu/RjButtons/RjButtons.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilgisayar_Toplama_Otomasyonu.RjButtons
+{
+    internal class RjButtons
+    {
+    }
+}
